f0cab5931945707321017ea0db3240c65d023ab5
Noah Yek <noah.yek@theaccessgroup.com>
2025-04-15 15:08:24 +0800

    Added incrementCounterHandler and decrementCounterHandler with the counter states and buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { useState } from "react";
+import { Image, StyleSheet, Button, Text, View } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -6,6 +7,16 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounterHandler = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const decrementCounterHandler = () => {
+    setCounter((prev) => prev - 1);
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -17,43 +28,20 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome Access</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View style={{ borderWidth: 0.3, borderRadius: 20, padding: 10 }}>
+        <Text style={{ fontWeight: "400", textAlign: "center", fontSize: 20 }}>
+          {counter}
+        </Text>
+        <View
+          style={{ flexDirection: "row", alignSelf: "center", marginTop: 10 }}
+        >
+          <Button title="Increment" onPress={incrementCounterHandler} />
+          <Button title="Decrement" onPress={decrementCounterHandler} />
+        </View>
+      </View>
     </ParallaxScrollView>
   );
 }
